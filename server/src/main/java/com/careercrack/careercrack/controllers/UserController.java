2abdf76cc87934846e238e973d1141228d95f747
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-01-04 16:49:08 -0600

    Add User controller

---
+++ server/src/main/java/com/careercrack/careercrack/controllers/UserController.java
@@ -0,0 +1,61 @@
+package com.careercrack.careercrack.controllers;
+import com.careercrack.careercrack.models.UserModel;
+import com.careercrack.careercrack.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserModel> getUserById(@PathVariable Long id){
+        Optional<UserModel> user = userService.findById(id);
+
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/userName/{userName}")
+    public ResponseEntity<UserModel> getUserByUserName(@PathVariable String userName){
+        UserModel user = userService.findByUserName(userName);
+
+        return (user != null) ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/userName/{userName}/email/{email}")
+    public ResponseEntity<UserModel> getUserByUserNameAndEmail(@PathVariable String userName, @PathVariable String email){
+        UserModel user = userService.findByUserNameAndEmail(userName, email);
+        return (user != null) ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/existsByEmail/{email}")
+    public ResponseEntity<Boolean> existsByEmail(@PathVariable String email){
+        Boolean exists = userService.existByEmail(email);
+        return ResponseEntity.ok(exists);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@Valid @RequestBody UserModel user){
+        userService.createUser(user);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        if (userService.findById(id).isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        userService.deleteUser(id);
+        return ResponseEntity.ok().build();
+    }
+
+
+}
