1f5df8831a79dd97bffb20fce565b59658f78737
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-01-08 21:04:16 -0600

    add getAllUsers controller method

--- server/src/main/java/com/careercrack/careercrack/controllers/UserController.java
+++ server/src/main/java/com/careercrack/careercrack/controllers/UserController.java
@@ -1,10 +1,14 @@
+// UserController.java
 package com.careercrack.careercrack.controllers;
+
 import com.careercrack.careercrack.models.UserModel;
 import com.careercrack.careercrack.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
+
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -12,50 +16,52 @@ import java.util.Optional;
 public class UserController {
     private final UserService userService;
 
-    public UserController(UserService userService){
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<UserModel> getUserById(@PathVariable Long id){
-        Optional<UserModel> user = userService.findById(id);
+    @GetMapping
+    public ResponseEntity<List<UserModel>> getAllUsers() {
+        List<UserModel> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<UserModel> getUserById(@PathVariable Long id) {
+        Optional<UserModel> user = userService.findById(id);
         return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/userName/{userName}")
-    public ResponseEntity<UserModel> getUserByUserName(@PathVariable String userName){
+    @GetMapping("/username/{userName}")
+    public ResponseEntity<UserModel> getUserByUserName(@PathVariable String userName) {
         UserModel user = userService.findByUserName(userName);
-
         return (user != null) ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
     }
 
-    @GetMapping("/userName/{userName}/email/{email}")
-    public ResponseEntity<UserModel> getUserByUserNameAndEmail(@PathVariable String userName, @PathVariable String email){
+    @GetMapping("/username/{userName}/email/{email}")
+    public ResponseEntity<UserModel> getUserByUserNameAndEmail(@PathVariable String userName, @PathVariable String email) {
         UserModel user = userService.findByUserNameAndEmail(userName, email);
         return (user != null) ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
     }
 
     @GetMapping("/existsByEmail/{email}")
-    public ResponseEntity<Boolean> existsByEmail(@PathVariable String email){
+    public ResponseEntity<Boolean> existsByEmail(@PathVariable String email) {
         Boolean exists = userService.existByEmail(email);
         return ResponseEntity.ok(exists);
     }
 
     @PostMapping
-    public ResponseEntity<Void> createUser(@Valid @RequestBody UserModel user){
+    public ResponseEntity<Void> createUser(@Valid @RequestBody UserModel user) {
         userService.createUser(user);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
-        if (userService.findById(id).isEmpty()){
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        if (userService.findById(id).isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         userService.deleteUser(id);
         return ResponseEntity.ok().build();
     }
-
-
 }
