44561424907f62aae68cabeca7fbb8e116239989
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-03-16 13:27:21 -0500

    remove unused OAuth2 resource server configuration

--- server/src/main/java/com/careercrack/careercrack/config/SecurityConfig.java
+++ server/src/main/java/com/careercrack/careercrack/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -19,9 +18,6 @@ public class SecurityConfig {
                     .anyRequest().authenticated()
             )
             .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .oauth2ResourceServer((oauth2) -> oauth2
-                    .jwt(Customizer.withDefaults())
-            )
             .httpBasic(Customizer.withDefaults())
             .build();
     }
