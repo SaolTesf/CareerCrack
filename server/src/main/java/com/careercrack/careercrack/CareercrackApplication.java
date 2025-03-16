419b56102a5855682ca0520768143a4f6bda424e
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-03-16 14:31:43 -0500

    add main application class and test class

---
+++ server/src/main/java/com/careercrack/careercrack/CareercrackApplication.java
@@ -0,0 +1,13 @@
+package com.careercrack.careercrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareercrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareercrackApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/careercrack/careercrack/CareercrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careercrack.careercrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareercrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
