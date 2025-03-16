92edb43ce700296f31f75c7cddb1e49fbca0d881
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-03-16 13:28:17 -0500

    add get all user and update user methods

--- server/src/main/java/com/careercrack/careercrack/services/UserService.java
+++ server/src/main/java/com/careercrack/careercrack/services/UserService.java
@@ -4,6 +4,7 @@ import com.careercrack.careercrack.models.UserModel;
 import com.careercrack.careercrack.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,6 +14,10 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public List<UserModel> getAllUsers() {
+        return userRepository.findAll();
     }
 
     public Optional<UserModel> findById(Long id) {
@@ -31,15 +36,24 @@ public class UserService {
         return userRepository.existsByEmail(email);
     }
 
-    public void createUser(UserModel user) {
-        userRepository.save(user);
+    public UserModel createUser(UserModel user) {
+        return userRepository.save(user);
+    }
+
+    public UserModel updateUser(Long id, UserModel user) {
+        UserModel existingUser = userRepository.findById(id).orElse(null);
+        if (existingUser != null) {
+            existingUser.setFirstName(user.getFirstName());
+            existingUser.setLastName(user.getLastName());
+            existingUser.setUsername(user.getUsername());
+            existingUser.setEmail(user.getEmail());
+            existingUser.setUpdatedAt(new Date());
+            return userRepository.save(existingUser);
+        }
+        return null;
     }
 
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
-    }
-
-    public List<UserModel> getAllUsers() {
-        return userRepository.findAll();
     }
 }
