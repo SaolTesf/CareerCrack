a6c53b0a2d173075471cac788b24f6a568ac4ffc
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-08-24 15:54:43 -0500

    refactor LoginSignup component to manage state for action and update button styles

--- client/src/Components/LoginSignUp/LoginSignup.jsx
+++ client/src/Components/LoginSignUp/LoginSignup.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginSignup.css'
 
 import { FaUser } from "react-icons/fa";
@@ -6,10 +6,14 @@ import { FaLock } from "react-icons/fa";
 
 
 export const LoginSignup = () => {
+
+  const [action, setAction] = useState("Sign Up");
+
+
   return (
     <div className='container'>
       <div className='header'>
-        <div className='text'>Signup</div>
+        <div className='text'>{action}</div>
         <div className='underline'></div>
       </div>
       <div className='form'>
@@ -24,8 +28,8 @@ export const LoginSignup = () => {
       </div>
       <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>
       <div className='submit-container'>
-        <div className='submit'>Sign Up</div>
-        <div className='submit'>Login</div>
+        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={() =>setAction("Login")}>Login</div>
       </div>
     </div>
   )
