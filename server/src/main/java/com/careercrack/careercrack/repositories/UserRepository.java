d8ca761f213101730ea070b8fcd97ce9ae6d8141
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-10-11 21:15:57 -0500

    add methods to UserRepository for retrieving users by username and email

--- server/src/main/java/com/careercrack/careercrack/repositories/UserRepository.java
+++ server/src/main/java/com/careercrack/careercrack/repositories/UserRepository.java
@@ -6,5 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     User findByUsernameAndEmail(String username, String email);
+
+    User getUserByUsername(String username);
+    User getUserByEmail(String email);
     boolean existsByEmail(String email);
 }
