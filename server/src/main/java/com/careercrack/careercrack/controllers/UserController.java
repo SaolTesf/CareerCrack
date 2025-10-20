769f3acab44701eb183a9688bc57c4764475ac80
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-10-20 18:27:42 -0500

    add logging to UserController.java

--- server/src/main/java/com/careercrack/careercrack/controllers/UserController.java
+++ server/src/main/java/com/careercrack/careercrack/controllers/UserController.java
@@ -2,8 +2,9 @@
 package com.careercrack.careercrack.controllers;
 
 import com.careercrack.careercrack.models.User;
-import com.careercrack.careercrack.services.JwtService;
 import com.careercrack.careercrack.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,58 +17,65 @@ import java.util.Optional;
 @RequestMapping("api/users")
 public class UserController {
     private final UserService userService;
-    private final JwtService jwtService;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    public UserController(UserService userService, JwtService jwtService) {
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.jwtService = jwtService;
     }
 
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = userService.getAllUsers();
+        logger.info("Fetched {} users", users.size());
         return ResponseEntity.ok(users);
     }
 
     @GetMapping("/id/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
         Optional<User> user = userService.findById(id);
+        logger.info("Fetching user with ID: {}", id);
         return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/username/{userName}")
-    public ResponseEntity<User> getUserByUserName(@PathVariable String userName) {
-        User user = userService.findByUserName(userName);
+    @GetMapping("/username/{username}")
+    public ResponseEntity<User> getUserByUserName(@PathVariable String username) {
+        User user = userService.findByUserName(username);
+        logger.info("Fetching user with username: {}", username);
         return (user != null) ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
     }
 
-    @GetMapping("/{userName}/{email}")
-    public ResponseEntity<User> getUserByUserNameAndEmail(@PathVariable String userName, @PathVariable String email) {
-        User user = userService.findByUserNameAndEmail(userName, email);
+    @GetMapping("/{username}/{email}")
+    public ResponseEntity<User> getUserByUserNameAndEmail(@PathVariable String username, @PathVariable String email) {
+        User user = userService.findByUserNameAndEmail(username, email);
+        logger.info("Fetching user with username: {} and email: {}", username, email);
         return (user != null) ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
     }
 
     @GetMapping("/email-check/{email}")
     public ResponseEntity<Boolean> existsByEmail(@PathVariable String email) {
         Boolean exists = userService.existByEmail(email);
+        logger.info("Email existence check for {}: {}", email, exists);
         return ResponseEntity.ok(exists);
     }
 
     @GetMapping("/username-check/{username}")
     public ResponseEntity<Boolean> existByUsername(@PathVariable String username) {
         Boolean exist = userService.existByUsername(username);
+        logger.info("Username existence check for {}: {}", username, exist);
         return ResponseEntity.ok(exist);
     }
 
     @PostMapping
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         User newUser = userService.createUser(user);
+        logger.info("Created new user with ID: {}", newUser.getId());
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @Valid @RequestBody User user) {
         User updatedUser = userService.updateUser(id, user);
+        logger.info("Updating user with ID: {}", id);
         return (updatedUser != null) ? ResponseEntity.ok(updatedUser) : ResponseEntity.notFound().build();
     }
 
@@ -77,6 +85,7 @@ public class UserController {
             return ResponseEntity.notFound().build();
         }
         userService.deleteUser(id);
+        logger.info("Deleted user with ID: {}", id);
         return ResponseEntity.ok().build();
     }
 }
