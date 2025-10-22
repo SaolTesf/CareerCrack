112d8c1b0b9a1523ab28c69e4e4aa2c1276d0de6
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-10-21 19:19:27 -0500

    add JwtAuthFilter.java

---
+++ server/src/main/java/com/careercrack/careercrack/config/JwtAuthFilter.java
@@ -0,0 +1,58 @@
+package com.careercrack.careercrack.config;
+
+import com.careercrack.careercrack.services.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JwtService jwtService;
+
+    public JwtAuthFilter(UserDetailsServiceImpl userDetailsService, JwtService jwtService) {
+        this.userDetailsService = userDetailsService;
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) {
+        try {
+            // Extract JWT from Authorization header
+            String authHeader = request.getHeader("Authorization");
+            String token = null;
+            String username = null;
+
+            // Check for Bearer token which is the standard for JWT
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                token = authHeader.substring(7);
+                username = jwtService.extractUsername(token);
+            }
+
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                // Load user details
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                if (jwtService.validateToken(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.getAuthorities()
+                    );
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+            filterChain.doFilter(request, response);
+        }
+        catch(Exception e) {
+            logger.error("Cannot set user authentication: {}", e);
+        }
+    }
+}
