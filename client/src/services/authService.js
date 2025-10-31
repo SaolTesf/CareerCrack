3a3f94b0b0c417410c81d7db385a971dfa5caf01
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-10-30 21:00:03 -0500

    add login method in authService

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,5 +1,12 @@
 import { apiClient } from './apiClient'
 
 export const authService = {
-  
+  async login(credentials) {
+    const data = await apiClient.post('/auth/login', credentials);
+    if (data.token) {
+      localStorage.setItem('authToken', data.token);
+      localStorage.setItem('user', JSON.stringify(data.user));
+    }
+    return data;
+  },
 }
