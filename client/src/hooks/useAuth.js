feb6222404562a3c3c43aa8d2f37883e4e41d2da
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-11-01 21:49:29 -0500

    add states

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -2,5 +2,6 @@ import { useState } from 'react';
 import { authService } from '../services/authService';
 
 export const useAuth = () => {
-
+  const [action, setAction] = useState("Sign Up");
+  const { login, register, loading, error } = useAuth();
 };
