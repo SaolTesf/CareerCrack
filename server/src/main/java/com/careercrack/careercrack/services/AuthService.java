130bce214b075a2eab9d691c789ac6c1a60a6e6f
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-11-03 20:07:37 -0600

    add registration logic

--- server/src/main/java/com/careercrack/careercrack/services/AuthService.java
+++ server/src/main/java/com/careercrack/careercrack/services/AuthService.java
@@ -1,7 +1,9 @@
 package com.careercrack.careercrack.services;
 
+import com.careercrack.careercrack.controllers.AuthController;
 import com.careercrack.careercrack.models.User;
 import com.careercrack.careercrack.repositories.UserRepository;
+import com.careercrack.careercrack.exceptions.DuplicateResourceException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +11,12 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserService userService;
 
-    public AuthService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public AuthService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, UserService userService) {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.userService = userService;
     }
 
     public User login(String username, String password) {
@@ -21,5 +25,21 @@ public class AuthService {
             return user;
         }
         return null;
+    }
+
+    public User register(AuthController.RegisterRequest request) {
+        if(userService.existByEmail(request.getEmail())) {
+            throw new DuplicateResourceException("Email is already in use");
+        }
+        if(userService.existByUsername(request.getUsername())) {
+            throw new DuplicateResourceException("Username is already taken");
+        }
+
+        User user = new User();
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setUsername(request.getUsername());
+        user.setEmail(request.getEmail());
+        return userService.createUser(user);
     }
 }
