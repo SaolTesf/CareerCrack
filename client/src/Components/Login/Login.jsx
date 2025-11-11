65ce1362cb5220aecb1d543e4e9ff8da6c46bfc8
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-11-10 21:23:22 -0600

    update login to handle change and submit

--- client/src/Components/Login/Login.jsx
+++ client/src/Components/Login/Login.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import { FaUser, FaLock } from "react-icons/fa";
 
-export const Login = () => {
+export const Login = ({onSubmit, loading}) => {
+
+  const [formData, setFormData] = useState({ identifier: '', password: '' });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (onSubmit) {
+      await onSubmit(formData);
+    }
+  }
+
   return (
-    <div className="form">
+    <form className="form" onSubmit={handleSubmit} noValidate>
       <div className="input">
         <FaUser />
         <input 
           id="username-email"
           type="username/email"
           placeholder="username/email"
+          value={formData.identifier}
+          onChange={handleChange}
         />
       </div>
       <div className="input">
@@ -18,8 +35,16 @@ export const Login = () => {
           id="password"
           type="password"
           placeholder="password"
+          value={formData.password}
+          onChange={handleChange}
         />
       </div>
-    </div>
+      <div className="forgot-password">
+          Forgot Password? <span>Click Here!</span>
+      </div>
+      <div className='submit-container'>
+        <button type='submit'className="submit">Log In</button>
+      </div>
+    </form>
   )
 }
