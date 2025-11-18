bc0b6ce377cb560a3632cad10fa5a9cf1159d26e
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-11-17 19:30:26 -0600

    add cors configuration

--- server/src/main/java/com/careercrack/careercrack/config/CorsConfig.java
+++ server/src/main/java/com/careercrack/careercrack/config/CorsConfig.java
@@ -1,4 +1,27 @@
 package com.careercrack.careercrack.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+import java.util.Objects;
+
+@Configuration
 public class CorsConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(Environment environment) {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(Objects.requireNonNull(environment.getProperty("CLIENT_URLS"))));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
