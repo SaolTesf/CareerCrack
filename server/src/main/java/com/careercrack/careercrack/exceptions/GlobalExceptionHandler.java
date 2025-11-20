dd31cac5999fb07e478a0c62799a4f01a54481bb
Sawol Tesfaghebriel <sawoltesf@gmail.com>
2025-11-20 16:58:19 -0600

    add exception handler for invalid credentials

--- server/src/main/java/com/careercrack/careercrack/exceptions/GlobalExceptionHandler.java
+++ server/src/main/java/com/careercrack/careercrack/exceptions/GlobalExceptionHandler.java
@@ -18,6 +18,14 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
 
+    /* logging in with invalid credentials */
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<Map<String, String>> handleInvalidCredentials(InvalidCredentialsException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
+    }
+
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Map<String, String>> handleRuntime(RuntimeException ex) {
         Map<String, String> errorResponse = new HashMap<>();
