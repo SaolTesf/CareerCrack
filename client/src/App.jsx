dd0364b64e05dcb04d1e50e077ceb0ceee8587b5
Sawol Tesfaghebriel <saoltesf@gmail.com>
2025-11-25 20:38:50 -0600

    add BrowserRouter from react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
 import logo from './Components/Assets/CareerCrack-logo.png';
 import './App.css';
 import { LoginSignup } from './Components/LoginSignUp/LoginSignup';
 
 function App() {
   return (
+    <BrowserRouter>
     <div>
       <div className='logo-container'>
         <img src={logo} alt='CareerCrack logo' className='App-logo' />
       </div>
-      <LoginSignup></LoginSignup>
-    </div>
+    </BrowserRouter>
   );
 }
 
