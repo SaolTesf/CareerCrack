2ab428ebd7e94eb6a53e4a6dc0a8472892650293
Sawol Tesfaghebriel <saoltesf@gmail.com>
2025-11-29 21:53:30 -0600

    handle loading and navigation

--- client/src/routes/ProtectedRoute.jsx
+++ client/src/routes/ProtectedRoute.jsx
@@ -2,5 +2,13 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 export const ProtectedRoute = ( { children }) => {
-  
+  const  { isAuthenticated, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>;
+    // TODO: update to have a loading component
+  }
+
+  return isAuthenticated ? children : <Navigate to="/login" replace />
+
 }
