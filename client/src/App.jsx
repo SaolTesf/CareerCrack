738e7d02309ee2e47448a02050c66dbde88d2436
Saol Tesfaghebriel <saoltesf@gmail.com>
2025-12-04 22:04:38 -0600

    add Navbar to app jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,12 @@ import { ProtectedRoute } from './routes/ProtectedRoute';
 import { LoginSignup } from './Components/LoginSignUp/LoginSignup';
 import { Home } from './Components/Home/Home';
 import { AuthProvider } from './context/AuthContext';
-
+import { Navbar } from './components/NavBar/Navbar';
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
+        <Navbar/>
         <Routes>
           <Route path='/login' element={<LoginSignup />} />
           <Route 
