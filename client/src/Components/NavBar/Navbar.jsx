7f6f602f7f76de51355adec4a6fb8238d9bed828
Saol Tesfaghebriel <saoltesf@gmail.com>
2025-12-10 20:32:28 -0600

    add dynamic styling to navbar links

--- client/src/Components/NavBar/Navbar.jsx
+++ client/src/Components/NavBar/Navbar.jsx
@@ -1,24 +1,27 @@
 import { React } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FaHome, FaBriefcase, FaClipboardList, FaCog } from "react-icons/fa";
 import './Navbar.css';
 
 export const Navbar = () => {
+  const location = useLocation();
+  let selectedTab = location.pathname;
+
   return (
     <nav className='navbar'>
-      <Link to={"/home"}>
+      <Link to={"/home"} className={selectedTab==="/home" ? "selected": ''}>
         <FaHome />
         <span>Home</span>
       </Link>
-      <Link to={"/problems"}>
+      <Link to={"/problems"} className={selectedTab==="/problems" ? "selected": ''}>
         <FaClipboardList />
         <span>Problems</span>
       </Link>
-      <Link to={"/applications"}>
+      <Link to={"/applications"} className={selectedTab==="/applications" ? "selected": ''}>
         <FaBriefcase />
         <span>Applications</span>
       </Link>
-      <Link to={"/settings"}>
+      <Link to={"/settings"} className={selectedTab==="/settings" ? "selected": ''}>
         <FaCog />
         <span>Settings</span>
       </Link>
