50a9775b0a098c6a6ee0fcc8f9f1cc51217c1fa9
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-01-17 19:26:25 -0600

    add persistent annotations for attributes

--- server/src/main/java/com/careercrack/careercrack/models/ProblemCategories.java
+++ server/src/main/java/com/careercrack/careercrack/models/ProblemCategories.java
@@ -1,7 +1,32 @@
 package com.careercrack.careercrack.models;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "problem_categories", schema = "careercrack")
 public class ProblemCategories {
+
+    @Id
+    @Column(name = "id", unique = true, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "name", unique = true)
+    @NotNull
+    @Size(min = 1, max = 50)
     private String name;
-    private String text;
+
+    @Column(name = "description")
+    private String description;
 }
