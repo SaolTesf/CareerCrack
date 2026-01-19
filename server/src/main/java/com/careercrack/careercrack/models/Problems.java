d00174d073c8f45ca32abe5f5180daa8332e81ab
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-01-18 21:05:03 -0600

    add lombok annotation for class

--- server/src/main/java/com/careercrack/careercrack/models/Problems.java
+++ server/src/main/java/com/careercrack/careercrack/models/Problems.java
@@ -3,11 +3,21 @@ package com.careercrack.careercrack.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.util.Date;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "problems", schema = "careercrack")
 public class Problems {
 
     public enum Status {
