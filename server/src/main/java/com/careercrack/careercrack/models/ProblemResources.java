43b32236633b2e48985ded86a61f62618f126201
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-01-19 18:37:22 -0600

    create ProblemResources class

---
+++ server/src/main/java/com/careercrack/careercrack/models/ProblemResources.java
@@ -0,0 +1,32 @@
+package com.careercrack.careercrack.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "problem_resources", schema = "careercrack")
+public class ProblemResources {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, updatable = false)
+    private Long id;
+
+    @Column(name = "problem_id")
+    @NonNull
+    private Long problemId;
+
+    @Column(name = "resource_type")
+    @Size(max = 50)
+    private String resourceType;
+
+    @Column(name = "url")
+    private String url;
+
+    @Column(name = "description")
+    private String description;
+}
