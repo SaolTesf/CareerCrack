07752b12dc239ac187ea3c2cb6f9d843d1f5eab9
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-01-19 18:44:38 -0600

    create ProblemResources class

---
+++ server/src/main/java/com/careercrack/careercrack/models/Tags.java
@@ -0,0 +1,17 @@
+package com.careercrack.careercrack.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Tags {
+    @Id
+    @Column(name = "id", unique = true, updatable = false)
+    private Long id;
+
+    @Column(name = "name")
+    @Size(max = 50)
+    private String name;
+}
