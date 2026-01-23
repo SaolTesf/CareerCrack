df98c03fc341fd06e3b7f064e503fb5f99e1926e
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-01-22 21:07:56 -0600

    add Problem and mapping annotations

--- server/src/main/java/com/careercrack/careercrack/models/ProblemTag.java
+++ server/src/main/java/com/careercrack/careercrack/models/ProblemTag.java
@@ -1,8 +1,6 @@
 package com.careercrack.careercrack.models;
 
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,10 +11,17 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
-@Table(name = "problemTags", schema = "careercrack")
+@Table(name = "problem_tags", schema = "careercrack")
 public class ProblemTag {
 
     @EmbeddedId
     private ProblemTagId problemTagId;
+
+    // Many-to-one relationship to Tag
+    // FetchType.LAZY to avoid loading the Tag unless needed
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("problemId")
+    @JoinColumn(name = "problem_id", nullable = false)
+    private Problem problem;
 
 }
