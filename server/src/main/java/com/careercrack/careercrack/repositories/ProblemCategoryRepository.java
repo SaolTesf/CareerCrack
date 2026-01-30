79b4b8e5393cd039307807a365b5a92ac023177a
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-01-29 20:33:22 -0600

    create ProblemCategoryRepository.java

---
+++ server/src/main/java/com/careercrack/careercrack/repositories/ProblemCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.careercrack.careercrack.repositories;
+
+
+import com.careercrack.careercrack.models.ProblemCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProblemCategoryRepository extends JpaRepository<ProblemCategory, Long> {
+
+}
