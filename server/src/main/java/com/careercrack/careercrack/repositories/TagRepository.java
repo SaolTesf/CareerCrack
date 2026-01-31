f3e8a6993350442c3d7cc4167def9dbedc1e8f62
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-01-31 16:29:01 -0600

    create TagRepository.java

---
+++ server/src/main/java/com/careercrack/careercrack/repositories/TagRepository.java
@@ -0,0 +1,12 @@
+package com.careercrack.careercrack.repositories;
+
+import com.careercrack.careercrack.models.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByNameIgnoreCase(String name);
+
+    boolean existsByNameIgnoreCase(String name);
+}
