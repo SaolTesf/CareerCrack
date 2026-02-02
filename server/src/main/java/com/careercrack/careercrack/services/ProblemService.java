7ac1ea15e7d242e92e1ee06800f14465725ef40e
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-02-01 20:35:27 -0600

    create ProblemService.java

---
+++ server/src/main/java/com/careercrack/careercrack/services/ProblemService.java
@@ -0,0 +1,12 @@
+package com.careercrack.careercrack.services;
+
+import com.careercrack.careercrack.repositories.ProblemRepository;
+import com.careercrack.careercrack.models.Problem;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProblemService {
+}
