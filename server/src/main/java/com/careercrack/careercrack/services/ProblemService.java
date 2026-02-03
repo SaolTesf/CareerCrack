02f4862745e7b5c77ec114ce186d10e231a4d510
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-02-02 20:33:15 -0600

    add problemRepository to service and get methods

--- server/src/main/java/com/careercrack/careercrack/services/ProblemService.java
+++ server/src/main/java/com/careercrack/careercrack/services/ProblemService.java
@@ -9,4 +9,17 @@ import java.util.Optional;
 
 @Service
 public class ProblemService {
+    private final ProblemRepository problemRepository;
+
+    public ProblemService(ProblemRepository problemRepository) {
+        this.problemRepository = problemRepository;
+    }
+
+    public List<Problem> getAllProblems() {
+        return problemRepository.findAll();
+    }
+
+    public Optional<Problem> findById(Long id) {
+        return problemRepository.findById(id);
+    }
 }
