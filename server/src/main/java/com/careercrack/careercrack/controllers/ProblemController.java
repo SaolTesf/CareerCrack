3cc0a8e83a1ec95bbd22eea2125f5712c873b74c
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-02-04 17:26:54 -0600

    create ProblemController.java and getAllProducts with pagination

---
+++ server/src/main/java/com/careercrack/careercrack/controllers/ProblemController.java
@@ -0,0 +1,33 @@
+package com.careercrack.careercrack.controllers;
+
+import com.careercrack.careercrack.models.Problem;
+import com.careercrack.careercrack.services.ProblemService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/problems")
+public class ProblemController {
+    private final ProblemService problemService;
+
+    public ProblemController(ProblemService problemService) {
+        this.problemService = problemService;
+    }
+
+    @GetMapping
+    public Page<Problem> getAllProducts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "true") boolean ascending) {
+        Sort sort = ascending ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        Pageable pageable = PageRequest.of(page, size, sort);
+        return problemService.getAllProblems(pageable);
+    }
+}
