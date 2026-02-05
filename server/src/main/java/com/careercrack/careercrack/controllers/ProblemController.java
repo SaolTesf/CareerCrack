0aa73f04cdaeb744bf2c1046e0bb1470c7d3a5ae
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-02-05 17:39:02 -0600

    add getProblemById controller method

--- server/src/main/java/com/careercrack/careercrack/controllers/ProblemController.java
+++ server/src/main/java/com/careercrack/careercrack/controllers/ProblemController.java
@@ -2,14 +2,14 @@ package com.careercrack.careercrack.controllers;
 
 import com.careercrack.careercrack.models.Problem;
 import com.careercrack.careercrack.services.ProblemService;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/problems")
@@ -31,4 +31,10 @@ public class ProblemController {
         Pageable pageable = PageRequest.of(page, size, sort);
         return problemService.getAllProblems(pageable);
     }
+
+    @GetMapping("/{id}")
+    public Optional<Problem> getProblemById(@PathVariable Long id) {
+        return problemService.findById(id);
+    }
+
 }
