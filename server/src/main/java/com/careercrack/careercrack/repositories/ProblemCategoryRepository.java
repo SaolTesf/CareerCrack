4c8031792985e3090ebfeec19420d52a48744a16
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-02-12 20:53:40 -0600

    create getAllProblemsByName method in ProblemCategoryRepository

--- server/src/main/java/com/careercrack/careercrack/repositories/ProblemCategoryRepository.java
+++ server/src/main/java/com/careercrack/careercrack/repositories/ProblemCategoryRepository.java
@@ -1,9 +1,16 @@
 package com.careercrack.careercrack.repositories;
 
 
+import com.careercrack.careercrack.models.Problem;
 import com.careercrack.careercrack.models.ProblemCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProblemCategoryRepository extends JpaRepository<ProblemCategory, Long> {
 
+    @Query("SELECT p FROM Problem p JOIN ProblemCategory pc on p.categoryId = pc.id WHERE pc.name = :name")
+    Page<Problem> getAllProblemsByName(@Param("name") String name, Pageable pageable);
 }
