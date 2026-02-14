fb98d210e1c46be07742dff69695a585bd826c30
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-02-13 21:05:22 -0600

    add getAllProblems service method

---
+++ server/src/main/java/com/careercrack/careercrack/services/ProblemCategoryService.java
@@ -0,0 +1,20 @@
+package com.careercrack.careercrack.services;
+
+import com.careercrack.careercrack.models.Problem;
+import com.careercrack.careercrack.repositories.ProblemCategoryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProblemCategoryService {
+    private final ProblemCategoryRepository problemCategoryRepository;
+
+    public ProblemCategoryService(ProblemCategoryRepository problemCategoryRepository) {
+        this.problemCategoryRepository = problemCategoryRepository;
+    }
+
+    public Page<Problem> getAllProblems(String name, Pageable pageable) {
+        return problemCategoryRepository.getAllProblemsByName(name, pageable);
+    }
+}
