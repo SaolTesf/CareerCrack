221afa7a80f2692917519d0e52d68401cc20e8d1
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-02-15 16:12:26 -0600

    add RequestMapping annotation to ProblemCategoryController

--- server/src/main/java/com/careercrack/careercrack/controllers/ProblemCategoryController.java
+++ server/src/main/java/com/careercrack/careercrack/controllers/ProblemCategoryController.java
@@ -1,7 +1,9 @@
 package com.careercrack.careercrack.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/problem-categories")
 public class ProblemCategoryController {
 }
