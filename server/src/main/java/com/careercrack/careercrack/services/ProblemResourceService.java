25a279fd26297c624847eaa1c222ef3c6ee16365
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-02-18 20:54:12 -0600

    add constructor to ProblemResourceService for repository injection

--- server/src/main/java/com/careercrack/careercrack/services/ProblemResourceService.java
+++ server/src/main/java/com/careercrack/careercrack/services/ProblemResourceService.java
@@ -6,4 +6,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ProblemResourceService {
+    private final ProblemResourceRepository problemResourceRepository;
+
+    public ProblemResourceService(ProblemResourceRepository problemResourceRepository) {
+        this.problemResourceRepository = problemResourceRepository;
+    }
 }
