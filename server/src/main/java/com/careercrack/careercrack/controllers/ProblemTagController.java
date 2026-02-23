a5747e4f7064ae21667d3fc8a07374dc7fff7b4e
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-02-22 19:41:29 -0600

    create ProblemTagController controller

---
+++ server/src/main/java/com/careercrack/careercrack/controllers/ProblemTagController.java
@@ -0,0 +1,9 @@
+package com.careercrack.careercrack.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("problem-tags")
+public class ProblemTagController {
+}
