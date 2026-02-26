800eb4254513884f8fe6050b71b91fcfba4fbff0
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-02-25 20:59:41 -0600

    add createProblemResource method

--- server/src/main/java/com/careercrack/careercrack/controllers/ProblemResourceController.java
+++ server/src/main/java/com/careercrack/careercrack/controllers/ProblemResourceController.java
@@ -1,13 +1,24 @@
 package com.careercrack.careercrack.controllers;
 
+import com.careercrack.careercrack.models.ProblemResource;
 import com.careercrack.careercrack.repositories.ProblemResourceRepository;
+import com.careercrack.careercrack.services.ProblemResourceService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProblemResourceController {
-    private final ProblemResourceRepository problemResourceRepository;
+    private final ProblemResourceService problemResourceService;
 
-    public ProblemResourceController(ProblemResourceRepository problemResourceRepository) {
-        this.problemResourceRepository = problemResourceRepository;
+    public ProblemResourceController(ProblemResourceService problemResourceService) {
+        this.problemResourceService = problemResourceService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProblemResource> createProblemResource(ProblemResource problemResource) {
+        ProblemResource newProblemResource = problemResourceService.createProblemResource(problemResource);
+        return new ResponseEntity<>(newProblemResource, HttpStatus.CREATED);
     }
 }
