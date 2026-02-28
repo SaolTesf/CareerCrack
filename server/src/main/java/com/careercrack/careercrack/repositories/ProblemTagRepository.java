3dfb34970c85c59e2f40b02163e566e607eb3102
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-02-27 18:29:06 -0600

    add findAllProblemsById method to ProblemTagRepository

--- server/src/main/java/com/careercrack/careercrack/repositories/ProblemTagRepository.java
+++ server/src/main/java/com/careercrack/careercrack/repositories/ProblemTagRepository.java
@@ -1,8 +1,13 @@
 package com.careercrack.careercrack.repositories;
 
+import com.careercrack.careercrack.models.Problem;
 import com.careercrack.careercrack.models.ProblemTag;
 import com.careercrack.careercrack.models.ProblemTagId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +15,7 @@ public interface ProblemTagRepository extends JpaRepository<ProblemTag, ProblemT
     Boolean existsByProblem_IdAndTag_Id(Long problemId, Long tagId);
 
     void deleteByProblem_Id(Long problemId);
+
+    @Query("SELECT p FROM Problem p JOIN ProblemTag pt WHERE pt.tag.id = :tagId")
+    Page<Problem> findAllProblemsById(@Param("tagId") Long id, Pageable pageable);
 }
