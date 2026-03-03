84701255e72391fb7213e7c1912d25581a2ff0f9
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-03-02 20:19:13 -0600

    add Getter and Setter annotation

--- server/src/main/java/com/careercrack/careercrack/models/Tag.java
+++ server/src/main/java/com/careercrack/careercrack/models/Tag.java
@@ -3,8 +3,12 @@ package com.careercrack.careercrack.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "tags", schema = "careercrack")
 public class Tag {
     @Id
