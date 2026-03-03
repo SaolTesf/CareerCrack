6f090e073f01b2ffc55d6194ec674e6354427520
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-03-02 20:10:48 -0600

    create TagService.java

---
+++ server/src/main/java/com/careercrack/careercrack/services/TagService.java
@@ -0,0 +1,12 @@
+package com.careercrack.careercrack.services;
+
+import com.careercrack.careercrack.models.Tag;
+import com.careercrack.careercrack.repositories.TagRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TagService {
+
+}
