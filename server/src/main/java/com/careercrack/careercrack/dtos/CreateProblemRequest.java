16174b5e6f6a5ffdcfaf52497c048a84c3908d7a
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-03-07 20:53:01 -0600

    create CreateProblemRequest dto

---
+++ server/src/main/java/com/careercrack/careercrack/dtos/CreateProblemRequest.java
@@ -0,0 +1,42 @@
+package com.careercrack.careercrack.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CreateProblemRequest {
+
+    @NotNull
+    private Long userId;
+
+    @NotNull
+    private Long categoryId;
+
+    @NotBlank
+    @Size(min = 5, max = 255)
+    private String title;
+
+    private String externalLink;
+
+    @Size(max = 10)
+    private String difficulty;
+
+    @Size(min = 4, max = 20)
+    private String status;
+
+    private String description;
+
+    private String solution;
+
+    private List<String> tags;
+}
