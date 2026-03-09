ef16f9409e3727a6e49392962c2104d70e93cf8e
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-03-08 20:12:45 -0500

    create findById method for ProblemCategoryService

--- server/src/main/java/com/careercrack/careercrack/services/ProblemCategoryService.java
+++ server/src/main/java/com/careercrack/careercrack/services/ProblemCategoryService.java
@@ -1,10 +1,13 @@
 package com.careercrack.careercrack.services;
 
 import com.careercrack.careercrack.models.Problem;
+import com.careercrack.careercrack.models.ProblemCategory;
 import com.careercrack.careercrack.repositories.ProblemCategoryRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProblemCategoryService {
@@ -16,5 +19,9 @@ public class ProblemCategoryService {
 
     public Page<Problem> getAllProblems(String name, Pageable pageable) {
         return problemCategoryRepository.findAllProblemsByName(name, pageable);
+    }
+
+    public Optional<ProblemCategory> findById(Long id) {
+        return problemCategoryRepository.findById(id);
     }
 }
