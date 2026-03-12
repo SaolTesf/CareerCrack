e7055d87f2dec77f0adf3df268c2c2d0d3662f5c
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-03-11 20:29:12 -0500

    create UpdateProblemRequest dto

---
+++ server/src/main/java/com/careercrack/careercrack/dtos/UpdateProblemRequest.java
@@ -0,0 +1,33 @@
+package com.careercrack.careercrack.dtos;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UpdateProblemRequest {
+
+    @Size(min = 5, max = 255)
+    private String title;
+
+    private String externalLink;
+
+    @Size(max = 10)
+    private String difficulty;
+
+    @Size(min = 4, max = 20)
+    private String status;
+
+    private String description;
+
+    private String solution;
+
+    private List<String> tags;
+}
