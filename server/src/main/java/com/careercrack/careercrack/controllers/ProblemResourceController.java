1a2ad7fd572fdb41ea7004eb01bc009c8bd70877
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-05-17 20:07:19 -0500

    add propert structure for ProblemResourceController

--- server/src/main/java/com/careercrack/careercrack/controllers/ProblemResourceController.java
+++ server/src/main/java/com/careercrack/careercrack/controllers/ProblemResourceController.java
@@ -1,14 +1,17 @@
 package com.careercrack.careercrack.controllers;
 
 import com.careercrack.careercrack.models.ProblemResource;
-import com.careercrack.careercrack.repositories.ProblemResourceRepository;
 import com.careercrack.careercrack.services.ProblemResourceService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequestMapping("/api/problem-resources")
 public class ProblemResourceController {
     private final ProblemResourceService problemResourceService;
 
@@ -19,6 +22,7 @@ public class ProblemResourceController {
     @PostMapping
     public ResponseEntity<ProblemResource> createProblemResource(ProblemResource problemResource) {
         ProblemResource newProblemResource = problemResourceService.createProblemResource(problemResource);
+        log.info("Created problem resource for problem with ID {}", newProblemResource.getProblemId());
         return new ResponseEntity<>(newProblemResource, HttpStatus.CREATED);
     }
 }
