993017cdb489f4ff9d90c871bf4ed76c68be4624
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-05-17 21:27:16 -0500

    add constructors to Tag class

--- server/src/main/java/com/careercrack/careercrack/models/Tag.java
+++ server/src/main/java/com/careercrack/careercrack/models/Tag.java
@@ -3,12 +3,16 @@ package com.careercrack.careercrack.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "tags", schema = "careercrack")
 public class Tag {
     @Id
