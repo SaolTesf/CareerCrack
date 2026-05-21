05fba465420e06bc6d302fb4360266fc73d2e1e8
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-05-20 19:59:38 -0500

    create findAllByUserId method in ProblemRepository

--- server/src/main/java/com/careercrack/careercrack/repositories/ProblemRepository.java
+++ server/src/main/java/com/careercrack/careercrack/repositories/ProblemRepository.java
@@ -2,7 +2,9 @@ package com.careercrack.careercrack.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.careercrack.careercrack.models.Problem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
-
+    Page<Problem> findAllByUserId(Long userId, Pageable pageable);
 }
