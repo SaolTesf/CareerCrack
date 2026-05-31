0104bcb2b5994c8da5fc7999dc35479c85076e6e
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-05-30 20:55:21 -0500

    update import statements to be consistent with other dtos

--- server/src/main/java/com/careercrack/careercrack/dtos/ProblemResponse.java
+++ server/src/main/java/com/careercrack/careercrack/dtos/ProblemResponse.java
@@ -1,6 +1,9 @@
 package com.careercrack.careercrack.dtos;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
