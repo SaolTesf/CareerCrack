ec190a8b5402f355bef81abe1ae9438c6112b0fc
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-06-04 20:43:12 -0500

    catch a possible race condition for finding for creating tags

--- server/src/main/java/com/careercrack/careercrack/services/TagService.java
+++ server/src/main/java/com/careercrack/careercrack/services/TagService.java
@@ -2,6 +2,7 @@ package com.careercrack.careercrack.services;
 
 import com.careercrack.careercrack.models.Tag;
 import com.careercrack.careercrack.repositories.TagRepository;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,11 +20,17 @@ public class TagService {
     }
 
     public Tag findOrCreate(String name) {
-        return tagRepository.findByNameIgnoreCase(name
-        ).orElseGet(() -> {
-            Tag tag = new Tag();
-            tag.setName(name.toLowerCase());
-            return tagRepository.save(tag);
+        return tagRepository.findByNameIgnoreCase(name).orElseGet(() -> {
+            try{
+                Tag tag = new Tag();
+                tag.setName(name.toLowerCase());
+                return tagRepository.save(tag);
+            }
+            catch (DataIntegrityViolationException e) {
+                // If another thread saved it right before this one, just find it again
+                return tagRepository.findByNameIgnoreCase(name).orElseThrow(() -> new RuntimeException("Failed to find or create tag with same " + name));
+            }
+
         });
     }
 
