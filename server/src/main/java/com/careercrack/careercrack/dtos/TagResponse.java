682644b6f35d9b365dec9c462bb686153c5f3cc8
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-06-05 16:36:08 -0500

    create TagResponse.java

---
+++ server/src/main/java/com/careercrack/careercrack/dtos/TagResponse.java
@@ -0,0 +1,15 @@
+package com.careercrack.careercrack.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class TagResponse {
+    private Long id;
+    private String name;
+}
